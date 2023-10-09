3b4033b2fe8799eab11f8e66711d11cd0780e1ff
kdg0209 <s_cova_0418@naver.com>
2023-10-09 15:01:20 +0900

    1. 상품이 Sulfuras인 경우 sellIn과 quality는 변하지 않는다.
    2. 상품의 sellIn이 0보다 큰경우 quality를 증가할 수 있다

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -2,11 +2,15 @@ package com.gildedrose;
 
 public class Item {
 
+    private static final int MAX_QUALITY = 50;
+
+    private static final String AGED_BRIE_ITEM_NAME = "Aged Brie";
+    private static final String BACK_STAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS_ITEM = "Sulfuras, Hand of Ragnaros";
+
     public String name;
-
-    public int sellIn;
-
-    public int quality;
+    public int sellIn; // 아이템을 판매해야하는 (남은) 기간
+    public int quality; // 아이템의 가치
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -17,5 +21,17 @@ public class Item {
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    public void updateQuality() {
+        if (this.quality < MAX_QUALITY) {
+            this.quality++;
+        }
+    }
+
+    public void decreaseSellIn() {
+        if (!this.name.equals(SULFURAS_ITEM)) {
+            this.sellIn--;
+        }
     }
 }
